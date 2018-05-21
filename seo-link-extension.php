d9a06eee21be0e587f7a2d1b1b3ebd2af5ffc4a2
DanielFloeter <info@kometschuh.de>
2018-05-21 16:45:18 +0200

    WordPress Coding Standards

--- seo-link-extension.php
+++ seo-link-extension.php
@@ -50,8 +50,9 @@ function save_post_meta( $post_id, $post ) {
 	$post_type = get_post_type_object( $post->post_type );
 
 	/* Check if the current user has permission to edit the post. */
-	if ( ! current_user_can( $post_type->cap->edit_post, $post_id ) )
-	return $post_id;
+	if ( ! current_user_can( $post_type->cap->edit_post, $post_id ) ) {
+		return $post_id;
+	}
 
 	$url_options = array( 'url', 'target' );
 	foreach ( $url_options as $option ) {
@@ -118,12 +119,12 @@ function post_class_meta_box( $post ) { ?>
 function add_post_meta_boxes() {
 
 	add_meta_box(
-		'seo-link-extension',							// Unique ID
-		esc_html__( 'SEO Link Add-on', 'example' ),		// Title
-		__NAMESPACE__.'\post_class_meta_box',			// Callback function
-		'post',											// Admin page (or post type)
-		'side',											// Context
-		'default'										// Priority
+		'seo-link-extension', // Unique ID
+		esc_html__( 'SEO Link Add-on', 'example' ), // Title
+		__NAMESPACE__ . '\post_class_meta_box', // Callback function
+		'post', // Admin page (or post type)
+		'side', // Context
+		'default' // Priority
 	);
 }
 
@@ -135,15 +136,15 @@ function add_post_meta_boxes() {
 function post_meta_boxes_setup() {
 
 	/* Add meta boxes on the 'add_meta_boxes' hook. */
-	add_action( 'add_meta_boxes', __NAMESPACE__.'\add_post_meta_boxes' );
+	add_action( 'add_meta_boxes', __NAMESPACE__ . '\add_post_meta_boxes' );
 
 	/* Save post meta on the 'save_post' hook. */
-	add_action( 'save_post', __NAMESPACE__.'\save_post_meta', 10, 2 );
+	add_action( 'save_post', __NAMESPACE__ . '\save_post_meta', 10, 2 );
 }
 
 /* Fire our meta box setup function on the post editor screen. */
-add_action( 'load-post.php', __NAMESPACE__.'\post_meta_boxes_setup' );
-add_action( 'load-post-new.php', __NAMESPACE__.'\post_meta_boxes_setup' );
+add_action( 'load-post.php', __NAMESPACE__ . '\post_meta_boxes_setup' );
+add_action( 'load-post-new.php', __NAMESPACE__ . '\post_meta_boxes_setup' );
 
 
 /**
@@ -154,41 +155,36 @@ add_action( 'load-post-new.php', __NAMESPACE__.'\post_meta_boxes_setup' );
  *
  * @since 0.2
  */
-function title_link_filter($html,$widget,$instance) {
+function title_link_filter( $html, $widget, $instance ) {
 	global $post;
 
-	if (isset($instance['title_links']) && $instance['title_links'] == "no_links")
+	if ( isset($instance['title_links']) && $instance['title_links'] === "no_links" )
 	{
 		// remove href, if exist
-		if (preg_match('/href="[^"]+"/',$html))
-		{
-			$html = preg_replace('/href="[^"]+"/', "", $html);
+		if ( preg_match( '/href="[^"]+"/', $html ) ) {
+			$html = preg_replace( '/href="[^"]+"/', '', $html );
 		}
 
 		// change inline anchor to inline span element (start- and end tag)
-		$html = str_replace('<a ','<span ',$html);
-		$html = str_replace('</a>','</span>',$html);
-	}
-	else if (isset($instance['title_links']) && $instance['title_links'] == "custom_links")
-	{
+		$html = str_replace( '<a ', '<span ', $html );
+		$html = str_replace( '</a>', '</span>', $html );
+	} elseif ( isset( $instance['title_links'] ) && 'custom_links' === $instance['title_links'] ) {
 		// set new URL link
-		if (preg_match('/href="[^"]+"/',$html))
-		{
+		if ( preg_match( '/href="[^"]+"/', $html ) ) {
 			// retrieve the global notice for the current post;
 			$post_class = get_post_meta( $post->ID, 'post_url', true );
-			$html = preg_replace('/href="[^"]+"/', " href='" . $post_class . "' ", $html);
+			$html       = preg_replace( '/href="[^"]+"/', " href='" . $post_class . "' ", $html );
 		}
 
 		$post_target = get_post_meta( $post->ID, 'post_target', true );
-		if(isset($post_target) && $post_target)
-		{
-			$html = str_replace('<a ','<a target="_blank" ',$html);
+		if ( isset( $post_target ) && $post_target ) {
+			$html = str_replace( '<a ', '<a target="_blank" ', $html );
 		}
 	}
 	return $html;
 }
 
-add_filter('cpwp_post_html',__NAMESPACE__.'\title_link_filter',10,3);
+add_filter( 'cpwp_post_html', __NAMESPACE__ . '\title_link_filter', 10, 3 );
 
 
 /**
@@ -199,43 +195,43 @@ add_filter('cpwp_post_html',__NAMESPACE__.'\title_link_filter',10,3);
  *
  * @since 0.2
  */
-function search_engine_attribute_filter($html,$widget,$instance) {
+function search_engine_attribute_filter( $html, $widget, $instance ) {
 
-	if (isset($instance['search_engine_attribute']) && $instance['search_engine_attribute'] != 'none') {
+	if ( isset( $instance['search_engine_attribute']) && $instance['search_engine_attribute'] !== 'none' ) {
 		// remove old rel, if exist
-		if (preg_match('/(.*)rel=".*"(.*)/',$html))
-			$html = preg_replace('/rel=".*"/', "", $html);
+		if ( preg_match( '/(.*)rel=".*"(.*)/', $html ))
+			$html = preg_replace( '/rel=".*"/', '', $html );
 
 		// add attribute
-		switch ($instance['search_engine_attribute']) {
+		switch( $instance['search_engine_attribute'] ) {
 			case 'canonical':
-				$html = str_replace('<a ','<a rel="canonical" ',$html);
+				$html = str_replace( '<a ', '<a rel="canonical" ', $html );
 				break;
 			case 'nofollow':
-				$html = str_replace('<a ','<a rel="nofollow" ',$html);
+				$html = str_replace( '<a ', '<a rel="nofollow" ', $html );
 				break;
 		}
 	}
 	return $html;
 }
 
-add_filter('cpwp_post_html',__NAMESPACE__.'\search_engine_attribute_filter',10,3);
+add_filter( 'cpwp_post_html', __NAMESPACE__ . '\search_engine_attribute_filter', 10, 3 );
 
 
- /**
- * Check the Term and Category based Posts Widget version
- *
- *  @return Base widget supporteds this Extension version
- *
- */
+/**
+* Check the Term and Category based Posts Widget version
+*
+*  @return Base widget supporteds this Extension version
+*
+*/
 function version_check( $min_base_version = MINBASEVERSION ) {
-	$min_base_version = explode('.', $min_base_version);
+	$min_base_version = explode( '.', $min_base_version);
 
-	if ( !defined( '\termcategoryPostsPro\VERSION' ) ) return false;
-	$installed_base_version = explode('.', \termcategoryPostsPro\VERSION);
+	if ( ! defined( '\termcategoryPostsPro\VERSION' ) ) return false;
+	$installed_base_version = explode( '.', \termcategoryPostsPro\VERSION);
 
-	$ret = ($min_base_version[0] < $installed_base_version[0]) ||
-			($min_base_version[0] == $installed_base_version[0] && $min_base_version[1] <= $installed_base_version[1]);
+	$ret = ( $min_base_version[0] < $installed_base_version[0] ) ||
+			( $min_base_version[0] === $installed_base_version[0] && $min_base_version[1] <= $installed_base_version[1] );
 
 	return $ret;
 }
@@ -254,7 +250,7 @@ function version_notice() {
 	}
 }
 
-add_action( 'admin_notices', __NAMESPACE__.'\version_notice' );
+add_action( 'admin_notices', __NAMESPACE__ . '\version_notice' );
 
 /**
  * Panel "More Excerpt Options"
@@ -267,54 +263,54 @@ add_action( 'admin_notices', __NAMESPACE__.'\version_notice' );
  * @return true: override the widget panel
  *
  */
-function form_seo_panel_filter($widget,$instance) {
+function form_seo_panel_filter( $widget, $instance ) {
 
-	if ( ! version_check( "4.7.1" ) ) {
+	if ( ! version_check( '4.7.1' ) ) {
 		return;
 	}
 
-	$instance = wp_parse_args( ( array ) $instance, array(
+	$instance = wp_parse_args( (array) $instance, array(
 		// extension options
-		'search_engine_attribute'         => 'none',
-		'title_links'                     => 'default_links',
+		'search_engine_attribute' => 'none',
+		'title_links'             => 'default_links',
 	) );
 
 	// extension options
-	$search_engine_attribute         = $instance['search_engine_attribute'];
-	$title_links                     = $instance['title_links'];
+	$search_engine_attribute = $instance['search_engine_attribute'];
+	$title_links             = $instance['title_links'];
 
 	?>
-	<h4 data-panel="seo"><?php _e('SEO and Links Add-on','categorypostspro')?></h4>
+	<h4 data-panel="seo"><?php _e( 'SEO and Links Add-on', 'categorypostspro' )?></h4>
 	<div>
-		<?php if ( version_check( "4.7.1" ) ) : ?>
+		<?php if ( version_check( '4.7.1' ) ) : ?>
 		<p>
-			<label for="<?php echo $widget->get_field_id("title_links_default_links"); ?>">
-				<input type="radio" value="default_links" class="checkbox" id="<?php echo $widget->get_field_id("title_links_default_links"); ?>" name="<?php echo $widget->get_field_name("title_links"); ?>"<?php if($instance["title_links"] === 'default_links'){ echo 'checked="checked"'; }; ?> />
-				<?php _e( 'Normal WordPress URL','seo-link-extension' ); ?>
+			<label for="<?php echo $widget->get_field_id( 'title_links_default_links' ); ?>">
+				<input type="radio" value="default_links" class="checkbox" id="<?php echo $widget->get_field_id("title_links_default_links"); ?>" name="<?php echo $widget->get_field_name( 'title_links' ); ?>"<?php if( 'default_links' === $instance['title_links'] ){ echo 'checked="checked"'; }; ?> />
+				<?php _e( 'Normal WordPress URL', 'seo-link-extension' ); ?>
 			</label>
 		</p>
 		<p>
-			<label for="<?php echo $widget->get_field_id("title_links_no_links"); ?>">
-				<input type="radio" value="no_links" class="checkbox" id="<?php echo $widget->get_field_id("title_links_no_links"); ?>" name="<?php echo $widget->get_field_name("title_links"); ?>"<?php if($instance["title_links"] === 'no_links'){ echo 'checked="checked"'; }; ?> />
-				<?php _e( 'No links','seo-link-extension' ); ?>
+			<label for="<?php echo $widget->get_field_id( 'title_links_no_links' ); ?>">
+				<input type="radio" value='no_links' class="checkbox" id="<?php echo $widget->get_field_id( 'title_links_no_links' ); ?>" name="<?php echo $widget->get_field_name( 'title_links' ); ?>"<?php if( 'no_links' === $instance['title_links'] ){ echo 'checked="checked"'; }; ?> />
+				<?php _e( 'No links', 'seo-link-extension' ); ?>
 				<p class="howto">Use &lt;span&gt;</p>
 			</label>
 		</p>
 		<p>
-			<label for="<?php echo $widget->get_field_id("title_links_custom_links"); ?>">
-				<input type="radio" value="custom_links" class="checkbox" id="<?php echo $widget->get_field_id("title_links_custom_links"); ?>" name="<?php echo $widget->get_field_name("title_links"); ?>"<?php if($instance["title_links"] === 'custom_links'){ echo 'checked="checked"'; }; ?> />
-				<?php _e( 'Custom links','seo-link-extension' ); ?>
+			<label for="<?php echo $widget->get_field_id( 'title_links_custom_links' ); ?>">
+				<input type="radio" value="custom_links" class="checkbox" id="<?php echo $widget->get_field_id("title_links_custom_links"); ?>" name="<?php echo $widget->get_field_name( 'title_links' ); ?>"<?php if( 'custom_links' === $instance['title_links'] ){ echo 'checked="checked"'; }; ?> />
+				<?php _e( 'Custom links', 'seo-link-extension' ); ?>
 				<p class="howto">Set in post and page admin</p>
 			</label>
 		</p>
 		<?php endif; ?>
 		<p>
-			<label for="<?php echo $widget->get_field_id("search_engine_attribute"); ?>">
-				<?php _e( 'SEO friendly URLs:','seo-link-extension' ); ?>
-				<select id="<?php echo $widget->get_field_id("search_engine_attribute"); ?>" name="<?php echo $widget->get_field_name("search_engine_attribute"); ?>">
-					<option value="none" <?php selected($search_engine_attribute, 'none')?>><?php _e( 'None', 'category-posts' ); ?></option>
-					<option value="canonical" <?php selected($search_engine_attribute, 'canonical')?>><?php _e( 'canonical', 'category-posts' ); ?></option>
-					<option value="nofollow" <?php selected($search_engine_attribute, 'nofollow')?>><?php _e( 'nofollow', 'category-posts' ); ?></option>
+			<label for="<?php echo $widget->get_field_id( 'search_engine_attribute' ); ?>">
+				<?php _e( 'SEO friendly URLs:', 'seo-link-extension' ); ?>
+				<select id="<?php echo $widget->get_field_id( 'search_engine_attribute' ); ?>" name="<?php echo $widget->get_field_name( 'search_engine_attribute' ); ?>">
+					<option value="none" <?php selected( $search_engine_attribute, 'none')?>><?php _e( 'None', 'category-posts' ); ?></option>
+					<option value="canonical" <?php selected( $search_engine_attribute, 'canonical')?>><?php _e( 'canonical', 'category-posts' ); ?></option>
+					<option value="nofollow" <?php selected( $search_engine_attribute, 'nofollow')?>><?php _e( 'nofollow', 'category-posts' ); ?></option>
 				</select>
 			</label>
 		</p>
@@ -322,7 +318,7 @@ function form_seo_panel_filter($widget,$instance) {
 	<?php
 }
 
-add_filter('cpwp_after_general_panel',__NAMESPACE__.'\form_seo_panel_filter',10,5);
+add_filter( 'cpwp_after_general_panel', __NAMESPACE__ . '\form_seo_panel_filter', 10, 5 );
 
 /**
  * Filter for the shortcode settings
@@ -330,15 +326,15 @@ add_filter('cpwp_after_general_panel',__NAMESPACE__.'\form_seo_panel_filter',10,
  * @param shortcode settings
  *
  */
-function cpwp_default_settings($setting) {
+function cpwp_default_settings( $setting ) {
 
-	return wp_parse_args( ( array ) $setting, array(
-		'search_engine_attribute'         => 'none',
-		'title_links'                     => 'default_links',
+	return wp_parse_args( (array) $setting, array(
+		'search_engine_attribute' => 'none',
+		'title_links'             => 'default_links',
 	) );
 }
 
-add_filter('cpwp_default_settings',__NAMESPACE__.'\cpwp_default_settings');
+add_filter( 'cpwp_default_settings', __NAMESPACE__ . '\cpwp_default_settings' );
 
 // Plugin action links section
 
@@ -349,14 +345,12 @@ add_filter('cpwp_default_settings',__NAMESPACE__.'\cpwp_default_settings');
  *
  *  @since 0.1
  */
-function add_action_links ( $links ) {
-    $pro_link = array(
-        '<a target="_blank" href="http://tiptoppress.com/term-and-category-based-posts-widget/?utm_source=widget_seoext&utm_campaign=get_pro_seoext&utm_medium=action_link">'.__('Get the pro widget needed for this add-on','category-posts').'</a>',
-    );
-
-	$links = array_merge($pro_link, $links);
-
-    return $links;
+function add_action_links( $links ) {
+	$pro_link = array(
+		'<a target="_blank" href="http://tiptoppress.com/term-and-category-based-posts-widget/?utm_source=widget_seoext&utm_campaign=get_pro_seoext&utm_medium=action_link">' . __( 'Get the pro widget needed for this add-on', 'category-posts' ) . '</a>',
+	);
+	$links    = array_merge( $pro_link, $links );
+	return $links;
 }
 
-add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), __NAMESPACE__.'\add_action_links' );
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), __NAMESPACE__ . '\add_action_links' );
